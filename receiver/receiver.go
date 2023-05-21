3ee72604b837204299044cf4de11069ae6f6188c
yazdani <hosseinyazdani1376@gmail.com>
2023-05-21 16:26:44 +0330

    another go

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -1,108 +1,32 @@
-package receiver
+package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"sync"
-	"time"
 
-	"github.com/gorilla/websocket"
+	"socket/pkg"
 )
 
-var upgrader = websocket.Upgrader{}
-
-type Socket struct {
-	conn *websocket.Conn
-	mu   sync.Mutex
-}
-
-//func main() {
-//	u := url.URL{
-//		Scheme: "ws",
-//		Host:   "localhost:3000",
-//		Path:   "/",
-//	}
-//	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	defer c.Close()
-//	var counter int32
-//	var t = time.Now()
-//
-//	for {
-//		_, msg, err := c.ReadMessage()
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//
-//		counter++
-//		if counter%10000 == 0 {
-//			log.Println(counter, time.Since(t))
-//			fmt.Println(len(msg))
-//			t = time.Now()
-//		}
-//
-//
-//	}
-//}
-
-func Receiver() {
-	u := url.URL{
-		Scheme: "ws",
-		Host:   "localhost:3001",
-		Path:   "/",
-	}
-
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatalf("receiver failed: %s", err)
-	}
-	defer conn.Close()
-
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		c, err := upgrader.Upgrade(w, r, nil)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c, err := pkg.Upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Fatalf("Failed to upgrade WebSocket connection: %s", err)
+			log.Print("receiver upgrade:", err)
+			return
 		}
+
 		defer c.Close()
-
-		var counter int32
-		var t = time.Now()
-
 		for {
 			_, msg, err := c.ReadMessage()
 			if err != nil {
-				log.Fatal(err)
+				log.Println("receiver:", err)
+				break
 			}
-			counter++
-			if counter%10000 == 0 {
-				log.Println(counter, time.Since(t))
-				fmt.Println(len(msg))
-				t = time.Now()
-			}
-
-			socket := &Socket{
-				conn: c,
-			}
-
-			go worker(socket, msg)
+			log.Printf("recvied: %s", msg)
 		}
 	})
 
-	err = http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func worker(s *Socket, msg []byte) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	err := s.conn.WriteMessage(websocket.TextMessage, msg)
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
